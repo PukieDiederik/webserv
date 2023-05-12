c1a9707a826be2151de2b8f6d6952fa5c1aabb2b
Diederik Robert marie boeren <diederik.boeren@gmail.com>
2023-05-12 18:21:19 +0100

    Added Server and Route blocks to the config

--- include/ServerConfig.hpp
+++ include/ServerConfig.hpp
@@ -5,6 +5,45 @@
 #include <string>
 #include <vector>
 
+// Has config about routes
+struct RouteCfg {
+
+    bool is_redirect;
+    std::string redirect_to;
+
+    // This will be an absolute path
+    std::string root;
+
+    bool cgi_enabled;
+    bool directory_listing_enabled;
+    // If this is empty it will accept any method
+    std::vector<std::string> accepted_methods;
+
+    RouteCfg();
+    RouteCfg(const RouteCfg& copy);
+    ~RouteCfg();
+};
+
+// Has config about server blocks
+struct ServerCfg{
+    short port;
+    std::vector<std::string> server_names;
+
+    // First argument is the error code, the second argument is the path to a
+    // file. For the directory error page the error code should be -1.
+    std::map<short, std::string> error_pages;
+    int max_body_size;
+
+    // This should be an absolute path
+    std::string root_dir;
+    std::vector<RouteCfg> routes;
+
+    // Constructors/Destructor
+    ServerCfg();
+    ServerCfg(const ServerCfg& copy);
+    ~ServerCfg();
+};
+
 class ServerConfig {
 private:
     // A list of commands which will be used in 'cgi'. Having one collecion
@@ -18,8 +57,8 @@ private:
     // This will store mime types and their respective content-type. The first
     // argument is a file extensions, and the second argument is a content-type
     std::map<std::string, std::string> mime;
-    // SERVER
-    //   ROUTES
+    
+    std::vector<ServerCfg> servers;
 
     ServerConfig();
 public:
--- src/ServerConfig.cpp
+++ src/ServerConfig.cpp
@@ -1,6 +1,16 @@
 #include "ServerConfig.hpp"
 #include <string>
 
+
+RouteCfg::RouteCfg() { }
+RouteCfg::RouteCfg(const RouteCfg& copy) { (void)copy; }
+RouteCfg::~RouteCfg() { }
+
+ServerCfg::ServerCfg() { }
+ServerCfg::ServerCfg(const ServerCfg& copy) { (void)copy; }
+ServerCfg::~ServerCfg() { }
+
+// ServerConfig
 ServerConfig::ServerConfig(const std::string& file) { (void)file; }
 ServerConfig::ServerConfig(const ServerConfig& copy) { (void)copy; }
 
