d5f8732e490cb9802512b872809c1e4dc95ff264
Diederik Boeren <diederik.boeren@gmail.com>
2023-05-29 16:19:30 +0100

    Added A parsing exception

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 ## Files
 SHELL       = /bin/zsh
 
-FNAMES      = main.cpp HttpMessage.cpp HttpRequest.cpp ParsingUtils.cpp
+FNAMES      = main.cpp HttpMessage.cpp HttpRequest.cpp ParsingUtils.cpp ParsingException.cpp
 
 SRCS        = $(addprefix $(SRCS_DIR)/,$(FNAMES))
 OBJS        = $(addprefix $(OBJS_DIR)/,$(notdir $(FNAMES:.cpp=.o)))
--- src/HttpRequest.cpp
+++ src/HttpRequest.cpp
@@ -1,10 +1,8 @@
-#include <algorithm>
 #include <sstream>
 #include <iostream>
-#include <cstdlib>
 #include "HttpRequest.hpp"
 #include "ParsingUtils.hpp"
-
+#include "ParsingException.hpp"
 
 std::string HttpRequest::toStringStart() const
 {
@@ -23,28 +21,28 @@ HttpRequest::HttpRequest(const std::string& in) : HttpMessage()
         line = trimSpace(line);
 
         std::size_t method_end = line.find_first_of(' ');
-        if (method_end == std::string::npos) // TODO: throw exception
-            std::exit(1);
+        if (method_end == std::string::npos)
+            throw ParsingException("Not enough fields in header line");
         _method = line.substr(0, method_end);
 
         std::size_t target_end = line.find_first_of(' ', method_end + 1);
-        if (method_end == std::string::npos) // TODO: throw exception
-            std::exit(1);
+        if (target_end == std::string::npos)
+            throw ParsingException("Not enough fields in header line");
         _target = line.substr(method_end + 1, target_end - method_end - 1);
 
         // Checks if there are more fields than expected
-        if (line.find_first_of(' ', target_end + 1) != std::string::npos) // TODO: throw exception
-            std::exit(1);
+        if (line.find_first_of(' ', target_end + 1) != std::string::npos)
+            throw ParsingException("Too many fields in header line");
 
         std::string tmp = line.substr(target_end + 1);
-        if (tmp.find("HTTP/") != 0) // TODO: throw exception
-            std::exit(1);
+        if (tmp.find("HTTP/") != 0)
+            throw ParsingException("Invalid http version specifier");
         std::stringstream version_ss(tmp.substr(5));
-        if (!std::isdigit(version_ss.peek())) // TODO: throw exception
-            std::exit(1);
+        if (!std::isdigit(version_ss.peek()))
+            throw ParsingException("Invalid http version specifier");
         version_ss >> this->_major_version;
-        if (version_ss.get() != '.' || !std::isdigit(version_ss.peek())) // TODO: throw exception
-            std::exit(1);
+        if (version_ss.get() != '.' || !std::isdigit(version_ss.peek()))
+            throw ParsingException("Invalid http version specifier");
         version_ss >> this->_minor_version;
     }
 
@@ -53,14 +51,14 @@ HttpRequest::HttpRequest(const std::string& in) : HttpMessage()
         line = trimSpace(line);
         while (line != "")
         {
-            std::size_t separator = line.find_first_of(':');
+            std::size_t separator = line.find_first_of(": ");
             if (separator == std::string::npos)
-            // TODO: throw exception
-                std::exit(1);
+                throw ParsingException("Header field does not have seperator");
             std::string field_name = line.substr(0, separator);
             if (field_name.empty() ||
-                field_name.find_first_not_of(std::string("!#$%&'*+-.^_`|~") + ALPHA + DIGIT) != std::string::npos) // TODO: throw exception
-                std::exit(1);
+                field_name.find_first_not_of(std::string("!#$%&'*+-.^_`|~") + ALPHA + DIGIT) != std::string::npos)
+                throw ParsingException("Invalid characters in field name");
+
             std::string field_value = trimSpace(line.substr(separator + 1));
             add_header(field_name, field_value);
 
