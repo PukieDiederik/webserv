ef4822dda5c608b1373f76d76f222bed38c64fa6
gde-alme <goncalopes98@gmail.com>
2023-05-30 16:07:11 +0100

    new fork and branch

--- src/ServerConfig.cpp
+++ src/ServerConfig.cpp
@@ -1,6 +1,18 @@
 #include "ServerConfig.hpp"
 #include <string>
 
+/*
+ *	Config file parser:
+ *		1. Read line by line
+ *		2. Comments are defined by hastag (#). Everything from hastag to EOL is comment
+ *		3. Find block identifiers as keywords -> cgi; mime; server
+ *		4. Each block is confined between brackets -> keywd_bracket; subkeywd_bracket
+ *		5. Any configuration errors is thrown as exception:
+ *			- Unrecognized keyword / subkeyword (block identifier)
+ *			- Unclosed brackets
+ *			- Missing parameters
+ * */
+
 
 RouteCfg::RouteCfg() { }
 RouteCfg::RouteCfg(const RouteCfg& copy) { (void)copy; }
@@ -14,7 +26,9 @@ ServerCfg ServerCfg::operator=(const ServerCfg& copy) { (void)copy; return *this
 
 
 // ServerConfig
-ServerConfig::ServerConfig(const std::string& file) { (void)file; }
+ServerConfig::ServerConfig(const std::string& file) {
+	(void)file;
+}
 ServerConfig::ServerConfig(const ServerConfig& copy) { (void)copy; }
 
 ServerConfig::~ServerConfig() { }
