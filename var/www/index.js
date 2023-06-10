b7924eac9587adc11fbb1ec51a6e81fb18828968
Edgar alexandre Freire marques <efreire-@c1r19s15.42lisboa.com>
2023-06-10 19:39:48 +0100

    Create the running button functionality

--- var/www/index.js
+++ var/www/index.js
@@ -1,5 +1,28 @@
-function sayHello() {
-	alert("HELLO WORLD");
+function sayHello(message) {
+	alert(message || 'HELLO WORLD');
+}
+
+function appendRunningButton() {
+	window.addEventListener("DOMContentLoaded", (event) => {
+		const runningContainer = document.createElement('div');
+		runningContainer.classList.add('running-container');
+
+		const runningButton = document.createElement('button');
+		runningButton.innerText = 'TRY ME';
+		runningButton.onclick = () => { sayHello('CAUGHT ME :(') };
+		runningButton.classList.add('running-button');
+
+		runningButton.style.top = '1%';
+		runningButton.style.left = '1%';
+
+		runningContainer.addEventListener("mouseover", (event) => {
+			runningButton.style.top = `max(1%, calc(${(Math.random() * 100)}% - 45px - 1%))`;
+			runningButton.style.left = `max(1%, calc(${(Math.random() * 100)}% - 120px - 1%))`;
+		});
+
+		runningContainer.append(runningButton);
+		document.querySelector('.page-wrapper .container').append(runningContainer);
+	});
 }
 
 function appendReturnButtons() {
@@ -20,6 +43,10 @@ function appendReturnButtons() {
 
 		document.querySelector('.page-wrapper .container').append(buttons);
 	});
+}
+
+if (window.location.pathname == '/index-alpha.html') {
+	appendRunningButton();
 }
 
 appendReturnButtons();
