418d94481f9beca7e2d568a515125868ef788c8c
Edgar alexandre Freire marques <efreire-@c1r19s15.42lisboa.com>
2023-06-10 19:05:20 +0100

    Append the return buttons

--- var/www/index.js
+++ var/www/index.js
@@ -1,3 +1,25 @@
 function sayHello() {
 	alert("HELLO WORLD");
 }
+
+function appendReturnButtons() {
+	window.addEventListener("DOMContentLoaded", (event) => {
+		const backToAlpha = document.createElement('a');
+		backToAlpha.innerText = 'ALPHA';
+		backToAlpha.href = 'index-alpha.html';
+		backToAlpha.classList.add('back-button');
+
+		const backToBeta = document.createElement('a');
+		backToBeta.innerText = 'BETA';
+		backToBeta.href = 'index-beta.html';
+		backToBeta.classList.add('back-button');
+
+		const buttons = document.createElement('div');
+		buttons.classList.add('back-buttons');
+		buttons.append(backToAlpha, backToBeta);
+
+		document.querySelector('.page-wrapper .container').append(buttons);
+	});
+}
+
+appendReturnButtons();
