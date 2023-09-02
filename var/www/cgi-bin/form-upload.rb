1a45c2a9823de123ec8b520e8de0305ef141e48f
edgar marques <edgar.eu1@hotmail.com>
2023-09-02 12:56:23 +0100

    Update the CGI file 'form-upload.rb'

---
+++ var/www/cgi-bin/form-upload.rb
@@ -0,0 +1,38 @@
+# #!/usr/bin/ruby
+
+require "cgi"
+
+cgi = CGI.new
+params = cgi.params
+
+if params.has_key?"file"
+	file = params["file"].first
+	server_file = '/home/edgar/projects/42-school/webserver-fork/var/www/files/' + file.original_filename
+	File.open(server_file.untaint, "w") do |f|
+		f << file.read
+	end
+end
+
+puts 'HTTP/1.0 200 OK'
+puts 'Content-type: text/html'
+puts ''
+
+puts '<!DOCTYPE html>'
+puts '<html lang="en">'
+puts '	<head>'
+puts '		<title>42 Webserver - Form Submission</title>'
+puts '		<link rel="stylesheet" href="/index.css">'
+puts '		<script src="/index.js"></script>'
+puts '	</head>'
+puts '	<body>'
+puts '		<div class="page-wrapper">'
+puts '			<div class="container">'
+puts '				<section>'
+puts '					<h1 class="title">'
+puts '						Form Submission'
+puts '					</h1>'
+puts '				</section>'
+puts '			</div>'
+puts '		</div>'
+puts '	</body>'
+puts '</html>'
