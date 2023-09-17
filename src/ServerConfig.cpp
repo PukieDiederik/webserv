224cd6e61bf8b6a856f1f3774c06226ad8c566c8
Gonçalo Lopes <32138337+gde-alme@users.noreply.github.com>
2023-09-17 16:26:13 +0100

    Gde alme parser leaks fix (#74)
    * Fixed parser leaks
    
    * Fixed parser leaks #2
    
    * Update ServerConfig.cpp

--- src/ServerConfig.cpp
+++ src/ServerConfig.cpp
@@ -152,12 +152,36 @@ ServerConfig::~ServerConfig() {
 			delete[] (*it)[i];
 		delete[] *it;
 	}
+	_cgi_cmds.clear();
 }
 
 ServerConfig& ServerConfig::operator=(const ServerConfig& copy)
 {
     _mime = copy._mime;
     _servers = copy._servers;
+	
+	// deep copy _cgi_cmds
+	for ( size_t i = 0; i < copy._cgi_cmds.size(); i++ ) {
+		char**	og_cmd = copy._cgi_cmds[i];
+
+		size_t	len = 0;
+		while ( og_cmd[len] != NULL )
+			len++;
+		
+		char**	cmd_array = new char*[len + 1];
+		cmd_array[len] = NULL;
+
+		for ( size_t j = 0; j < len; j++ ) {
+			cmd_array[j] = new char[std::strlen( og_cmd[j] ) + 1];
+			std::strcpy( cmd_array[j], og_cmd[j] );
+		}
+
+		_cgi_cmds.push_back( cmd_array );
+
+	}
+
+	_cgi = copy._cgi;
+
     return *this;
 }
 
