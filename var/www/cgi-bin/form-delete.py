46d69aa28a0c4b3e5ae7d57d766f62a3ffd1fb3c
gde-alme <goncalopes98@gmail.com>
2023-09-26 17:37:24 +0100

    Added try blocks to cgi py scripts

--- var/www/cgi-bin/form-delete.py
+++ var/www/cgi-bin/form-delete.py
@@ -3,37 +3,40 @@
 import cgi
 import os
 
-form = cgi.FieldStorage()
+try:
+	form = cgi.FieldStorage()
 
-was_file_deleted = False
+	was_file_deleted = False
 
-if "filename" in form and form["filename"] and os.path.exists(form["filename"]):
-	try:
-		os.remove(form["filename"])
-		was_file_deleted = True
-	except Exception as e:
-		was_file_deleted = False
+	if "filename" in form and form["filename"] and os.path.exists(form["filename"]):
+		try:
+			os.remove(form["filename"])
+			was_file_deleted = True
+		except Exception as e:
+			was_file_deleted = False
 
-print('HTTP/1.0 200 OK')
-print('Content-type: text/html')
-print('')
+	print('HTTP/1.0 200 OK')
+	print('Content-type: text/html')
+	print('')
 
-print('<!DOCTYPE html>')
-print('<html lang="en">')
-print('	<head>')
-print('		<title>42 Webserver - Form Submission</title>')
-print('		<link rel="stylesheet" href="/index.css">')
-print('		<script src="/index.js"></script>')
-print('	</head>')
-print('	<body>')
-print('		<div class="page-wrapper">')
-print('			<div class="container">')
-print('				<section>')
-print('					<h1 class="title">')
-print("						File {}".format('deleted' if was_file_deleted else 'not deleted'))
-print('					</h1>')
-print('				</section>')
-print('			</div>')
-print('		</div>')
-print('	</body>')
-print('</html>')
+	print('<!DOCTYPE html>')
+	print('<html lang="en">')
+	print('	<head>')
+	print('		<title>42 Webserver - Form Submission</title>')
+	print('		<link rel="stylesheet" href="/index.css">')
+	print('		<script src="/index.js"></script>')
+	print('	</head>')
+	print('	<body>')
+	print('		<div class="page-wrapper">')
+	print('			<div class="container">')
+	print('				<section>')
+	print('					<h1 class="title">')
+	print("						File {}".format('deleted' if was_file_deleted else 'not deleted'))
+	print('					</h1>')
+	print('				</section>')
+	print('			</div>')
+	print('		</div>')
+	print('	</body>')
+	print('</html>')
+except Exception as e:
+	print("HTTP/1.0 500 Internal Server Error")
